4ed9913d9263e1f854cafcb22d1222d1b0dab2f5
Birri123 <l.terranova@alumno.um.edu.ar>
2022-10-17 22:01:45 -0300

    Testing2+README

--- test.py
+++ test.py
@@ -6,12 +6,61 @@ class Cuatro_LineUnittest(unittest.TestCase):
 
     def test_Fichax(self):
         
-        Cuatro_Linea().createTablero()
-        Cuatro_Linea().añadirF(1)
         juego = Cuatro_Linea()
-        juego.tablero
+        juego.createTablero()
+        juego.añadirF(1)
         self.assertEqual(juego.tablero[7][1], 'X')
 
-if __name__=='__main__':
+    def test_checkColumn_success(self):
 
+        juego = Cuatro_Linea()
+        juego.createTablero()
+        juego.añadirF(3)
+        self.assertEqual(juego.checkColum(3),6)
+
+    def test_checkColumn_full(self):
+
+        juego = Cuatro_Linea()
+        juego.createTablero()
+        
+        for i in range(8):
+            juego.añadirF(3)
+        self.assertEqual(juego.checkColum(3),-1)
+        
+
+    def test_cambioPlayer(self):
+        juego = Cuatro_Linea()
+        pass
+
+    def test_cambioTurno(self):
+        juego = Cuatro_Linea()
+        pass
+
+    def test_checkganador_1(self):
+        juego = Cuatro_Linea()
+        pass
+
+    def test_checkganador_2(self):
+        juego = Cuatro_Linea()
+        pass
+
+    def test_checkganador_3(self):
+        juego = Cuatro_Linea()
+        pass
+
+    def test_checkganador_4(self):
+        juego = Cuatro_Linea()
+        pass
+
+    def test_printTab(self):
+        juego = Cuatro_Linea()
+        pass
+
+    def test_turno(self):
+        juego = Cuatro_Linea()
+        pass
+          
+
+if __name__=='__main__':
+    
     unittest.main()
